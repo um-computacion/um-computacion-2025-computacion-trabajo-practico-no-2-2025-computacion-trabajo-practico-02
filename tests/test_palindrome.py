b505ec0e21d212592add3acd6693257d2c34ed6b
Facundo-Affar <f.affar@alumno.um.edu.ar>
2025-04-15 12:33:55 -0300

    Implementacion de los tests frases palindromas

--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -2,13 +2,20 @@ import unittest
 from src.palindrome import is_palindrome
 
 class TestPalindromos(unittest.TestCase):
+    def test_frases_palindromas(self):
+        self.assertTrue(is_palindrome("Dábale arroz a la zorra el abad"))
+        self.assertTrue(is_palindrome("A mamá Roma le aviva el amor a mamá"))
+        self.assertTrue(is_palindrome("Anita lava la tina"))    
     
     def test_palindromos_en_minusculas(self):
 
         self.assertTrue(is_palindrome("madam"))
-        self.assertTrue(is_palindrome("salas"))
-        self.assertTrue(is_palindrome("nivel"))
+        self.assertTrue(is_palindrome("reconocer"))
+        self.assertTrue(is_palindrome("anilina"))
         self.assertTrue(is_palindrome("radar"))
+    
+
+
 
 if __name__ == '__main__':
     unittest.main()
